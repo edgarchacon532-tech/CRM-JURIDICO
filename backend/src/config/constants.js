9d1fe26db80891ae2df05cdc5d2a414bbd2b2261
edgarchacon532-tech <edgarchacon532@gmail.com>
2026-02-07 10:20:58 -0500

    Create backend/src/config/constants.js with application constants

---
+++ backend/src/config/constants.js
@@ -0,0 +1,15 @@
+// Application constants
+
+const API_VERSION = 'v1';
+const APP_NAME = 'CRM JURIDICO';
+const SUPPORT_EMAIL = 'support@crmjuridico.com';
+const DEFAULT_LANGUAGE = 'en';
+const TIMEZONE = 'UTC';
+
+module.exports = {
+    API_VERSION,
+    APP_NAME,
+    SUPPORT_EMAIL,
+    DEFAULT_LANGUAGE,
+    TIMEZONE
+};
